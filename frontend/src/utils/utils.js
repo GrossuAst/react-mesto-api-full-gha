cfc7a6ffb9e0853ce97a641e5e042043c3c66538
Maxim Grossu <maxgrossu@yandex.ru>
2023-07-17 12:20:17 +0300

    Изменения адреса в apiConfig и auth.js

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,8 +1,8 @@
 // конфиг для API___________________________
 export const apiConfig = {
-    url: 'http://grossuast.mesto.nomoredomains.xyz',
+    url: 'https://api.grossuast.mesto.nomoredomains.xyz',
     headers: {
-      authorization: 'e900e361-a4f9-4167-b7d1-fcc078aa308a',
+      // authorization: 'e900e361-a4f9-4167-b7d1-fcc078aa308a',
       'Content-Type': 'application/json'
     }
   };
